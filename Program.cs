0570a22de25b42d1edfe9311759ff2f7dedeb942
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-07 08:24:59 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//*****************************************************************************
+//** 1760. Minimum Limit of Balls in a Bag    leetcode                       **
+//*****************************************************************************
+
+int minimumSize(int* nums, int numsSize, int maxOperations)
+{
+    int left = 1, right = nums[0];
+    for (int i = 1; i < numsSize; i++)
+    {
+        if (nums[i] > right)
+        {
+            right = nums[i];
+        }
+    }
+
+    while (left < right)
+    {
+        int mid = (left + right) / 2;
+        long long cnt = 0;
+
+        for (int i = 0; i < numsSize; i++)
+        {
+            cnt += (nums[i] - 1) / mid;
+            if (cnt > maxOperations)
+            {
+                left = mid + 1;
+                goto NEXT_ITERATION;
+            }
+        }
+
+        right = mid;
+
+    NEXT_ITERATION:
+        continue;
+    }
+    return left;
+}
